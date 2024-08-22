1eb5b7e82582df81c11de89cbe4138341b4be27f
Yeswanthraj0306 <yeswaraj0306@gmail.com>
2024-08-22 20:30:18 +0530

    Add files via upload

---
+++ weekDays1/Mobiles.java
@@ -0,0 +1,30 @@
+package weekDays1;
+
+public class Mobiles{
+	
+	public static void main(String[] args) {
+		
+		String mobileName="REALME";
+		String mobileModal="realme 9";
+		int mobileRate=15000;
+		char mobileLogo='R';
+		float discountRate=6.00005f;
+		double imbNo=123567878733798l;
+		boolean isDisplay=true;
+		long warrantlyDays=123456678123344l;
+		
+		System.out.println(mobileName);
+		System.out.println(mobileModal);
+		System.out.println(mobileRate);
+		System.out.println(mobileLogo);
+		System.out.println(discountRate);
+		System.out.println(imbNo);
+		System.out.println(isDisplay);
+		System.out.println(warrantlyDays);
+		
+		System.out.println(mobileName +"\n"+mobileModal+"\n"+mobileRate +"\n"+mobileLogo +"\n"+ discountRate+"\n" + imbNo+"\n" + isDisplay+"\n" + warrantlyDays);
+			
+		
+	}
+
+}
