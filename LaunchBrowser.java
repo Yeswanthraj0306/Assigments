8c4ec459739c3bbb06f2a5f196b766d85244e50b
Yeswanthraj0306 <yeswaraj0306@gmail.com>
2024-09-10 18:43:12 +0530

    Update LaunchBrowser.java

--- LaunchBrowser.java
+++ LaunchBrowser.java
@@ -3,7 +3,7 @@ package week3Day3;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LaunchBroweser {
+public class LaunchBrowser {
 
 	public static void main(String[] args) {
 
