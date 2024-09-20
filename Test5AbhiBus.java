a719ea928f3c8a78d96fd8805db595291cfd1e89
Yeswanthraj0306 <yeswaraj0306@gmail.com>
2024-09-20 21:15:11 +0530

    Test5AbhiBus.java

--- Test5AbhiBus.java
+++ Test5AbhiBus.java
@@ -21,7 +21,7 @@ public class Test5AbhiBus {
 		driver.findElement(By.xpath("//input[@placeholder='To Station']")).sendKeys("Bangalore");
 		Thread.sleep(1000);
 		driver.findElement(By.xpath("//div[@class='station-body col']")).click();
-		
+
 		driver.findElement(By.xpath("//div[@class='input-suffix col auto']/button[2]")).click();
 		Thread.sleep(1000);
 		driver.findElement(By.xpath("//div[@id='search-button']")).click();
@@ -29,39 +29,35 @@ public class Test5AbhiBus {
 		WebElement Busname = driver.findElement(By.xpath("//div[@class='operator-info col s12 m5 l5']/h5[1]"));
 		String Bus = Busname.getText();
 		System.out.println("The selected bus name is " + Bus);
-		
+
 		driver.findElement(By.xpath("//span[text()='Sleeper']")).click();
-		
+
 		WebElement seat = driver.findElement(By.xpath("(//div[@style='text-align: center;'])[3]//small"));
 		String seatavailable = seat.getText();
 		System.out.println("The Availableseats " + seatavailable);
-		
 
-		
 		driver.findElement(By.xpath("(//div[@class='row '])[2]/button")).click();
-		driver.findElement(By.xpath("(//button[@class='seat sleeper'])[2]")).click();
-		
+		driver.findElement(By.xpath("(//button[@class='seat sleeper'])[3]")).click();
+
 		driver.findElement(By.xpath("//a[@class='btn bus-info-btn text tertiary sm inactive button'][1]")).click();
 		driver.findElement(By.xpath("(//div[@class='label'])[1]/p[1]")).click();
 		Thread.sleep(1000);
 		driver.findElement(By.xpath("(//div[@class='label'])[1]/p[1]")).click();
-		
-		
-		WebElement seatname = driver.findElement(By.xpath("(//button[@class='seat sleeper'])[2]"));
+
+		WebElement seatname = driver.findElement(By.xpath("(//button[@class='seat sleeper'])[3]"));
 		String seatnames = seatname.getText();
 		System.out.println("The SeatName " + seatnames);
-	
+
 		WebElement cost = driver.findElement(By.xpath("//div[@class='seat-fare col auto']"));
 		String rupees = cost.getText();
 		System.out.println("The cost of Fares " + rupees);
-		
+
 		String title = driver.getTitle();
-		if (title.contains("Chennai")) {
+		if (title.contains("Bus")) {
 			System.out.println("verified");
 		} else {
 			System.out.println("not verified");
 		}
-		
-		
+		driver.close();
 	}
 }
